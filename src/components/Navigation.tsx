a1776e557c3df7362801bbabbcd2868e72682f7a
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2025-01-02 12:12:04 +0530

    fix errors

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 
 const drawerWidth = 240;
-const navItems = [['Expertise', 'expertise'], ['Github', 'github'],['Projects', 'projects'], ['Contact', 'contact']];
+const navItems = [['Expertise', 'expertise'], ['Github', 'github'],['Projects', 'projects'], ['Contact', 'contact'] ,  ["Resume", "resume"],];
 
 function Navigation({parentToChild, modeChange}: any) {
 
@@ -49,6 +49,11 @@ function Navigation({parentToChild, modeChange}: any) {
   const scrollToSection = (section: string) => {
     console.log(section)
     const expertiseElement = document.getElementById(section);
+      if (section === "resume") {
+        // Open the resume directly
+        window.open("/resume.pdf", "_blank");
+        return;
+      }
     if (expertiseElement) {
       expertiseElement.scrollIntoView({ behavior: 'smooth' });
       console.log('Scrolling to:', expertiseElement);  // Debugging: Ensure the element is found
